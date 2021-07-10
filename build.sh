719e96e6cbdea8fcaba616dd8f33ef9b29f3206f
the-eric-kwok <erickwok404@gmail.com>
2021-07-10 20:58:50 +0800

    update

--- build.sh
+++ build.sh
@@ -9,8 +9,10 @@ for pkg in $pkgs; do
     name=$(grep "Package:" ${pkg}/DEBIAN/control | sed -e 's/Package: //g')
     arch=$(grep "Architecture:" ${pkg}/DEBIAN/control | sed -e 's/Architecture: //g')
     echo "Building ${name}_${version}_${arch}.deb"
-    dpkg -b $pkg "${name}_${version}_${arch}.deb"
+    dpkg -b ${pkg} "${name}_${version}_${arch}.deb"
     mkdir -p ../files/${pkg}
-    mv "${name}_${version}_${arch}.deb" ../files/${pkg}
+    mv "${name}_${version}_${arch}.deb" "../files/${pkg}"
+    echo "`pwd`: copying control file"
+    cp ${pkg}/DEBIAN/control ../files/${pkg}
 done
 cd -
