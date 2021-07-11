bfe62ee703f21e0a1cbbfce1689d8fbc4a0c776b
the-eric-kwok <erickwok404@gmail.com>
2021-07-11 17:18:58 +0800

    update repo

--- build.sh
+++ build.sh
@@ -8,11 +8,16 @@ for pkg in $pkgs; do
     version=$(grep "Version:" ${pkg}/DEBIAN/control | sed -e 's/Version: //g')
     name=$(grep "Package:" ${pkg}/DEBIAN/control | sed -e 's/Package: //g')
     arch=$(grep "Architecture:" ${pkg}/DEBIAN/control | sed -e 's/Architecture: //g')
-    echo "Building ${name}_${version}_${arch}.deb"
-    dpkg -b ${pkg} "${name}_${version}_${arch}.deb"
-    mkdir -p ../files/${pkg}
-    mv "${name}_${version}_${arch}.deb" "../files/${pkg}"
-    echo "`pwd`: copying control file"
-    cp ${pkg}/DEBIAN/control ../files/${pkg}
+    grep "${name}_${version}" ../Packages > /dev/null
+    if [ $? -eq 1 ]; then
+        echo "Building ${name}_${version}_${arch}.deb"
+        dpkg -b ${pkg} "${name}_${version}_${arch}.deb"
+        mkdir -p ../files/${pkg}
+        mv "${name}_${version}_${arch}.deb" "../files/${pkg}"
+        echo "`pwd`: copying control file"
+        cp ${pkg}/DEBIAN/control ../files/${pkg}
+    else
+        echo "Skip ${name}_${version}_${arch}.deb, already built."
+    fi
 done
-cd -
+cd - > /dev/null
--- update_repo.sh
+++ update_repo.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+./build.sh
 rm Packages*
-./dpkg-scanpackages -m . /dev/null >Packages
+dpkg-scanpackages -m . > Packages
 bzip2 -k Packages
 echo ""
 cat Packages
